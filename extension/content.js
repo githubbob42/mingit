3bfe6e264a0329c3e9db8179bf567c7d0da9d63f
Yusuke Morita <ymorita@liquidframeworks.com>
2015-05-11 15:03:02 -0500

    add support for moving cards through transitions

--- extension/content.js
+++ extension/content.js
@@ -58,7 +58,7 @@ function injectLink($link, card) {
   $link.parent().append(link);
 }
 
-function updateDetails() {
+function updateDetails(options) {
   $('.js-issue-title').each(function (i, title) {
     var $title = $(title);
     var mingle = parseMingleInfo($title.text());
@@ -73,19 +73,57 @@ function updateDetails() {
       var header = `
         <div class="flex-table gh-header-meta">
           <div class="flex-table-item">
-            <a target="mingle" href="${ card.url }" class="mingit state tooltipped tooltipped-n" aria-label="View Mingle Card">
-              <span class="octicon octicon-link-external"></span> Mingle ${ card.type } #${ card.number }
-            </a>
+
+            <div class="left select-menu js-menu-container js-select-menu">
+              <button class="mingit btn btn-sm select-menu-button js-menu-target" type="button" aria-haspopup="true">
+                Mingle ${ card.type } #${ card.number }
+              </button>
+              <div class="select-menu-modal-holder js-menu-content js-navigation-container" aria-hidden="true">
+                <div class="select-menu-modal">
+                  <div class="select-menu-list mingit-transitions">
+                    <a target="mingle" href="${ card.url }" class="mingit select-menu-item js-navigation-item">
+                      <span class="select-menu-item-icon octicon octicon-link-external" style="color: inherit"></span>
+                      <div class="select-menu-item-text">
+                        <strong>View card in Mingle</strong>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
           <div class="flex-table-item flex-table-item-primary">${ properties }</div>
         </div>`;
 
       $('#partial-discussion-header').append(header);
       $title.text(card.name);
+
+      if (options.username && options.password) {
+        $('.mingit-transitions').prepend(card.transitions.map(function (transition) {
+          return `
+            <a data-id="${ transition.id }" data-card="${ card.number }" data-project="${ card.project }" class="mingit-transition select-menu-item js-navigation-item">
+              <div class="select-menu-item-text">
+                ${ transition.name }
+              </div>
+            </a>`;
+        }).join(''));
+      }
     });
 
     $('.discussion-item-labeled, .discussion-item-unlabeled').remove();
   });
+}
+
+function handleTransition(e) {
+  var $btn = $(e.currentTarget),
+      params = $btn.data();
+
+  chrome.runtime.sendMessage({ transition: params }, window.location.reload);
+
+  e.preventDefault();
+  e.stopPropagation();
+  return false;
 }
 
 function getContrastColor(hexcolor) {
@@ -105,11 +143,12 @@ function injectMingle() {
     if (!options.showLinks && !options.showLabels) return;
     if (!window.location.href.match(new RegExp(options.repo, 'i'))) return;
     updateList(options);
-    updateDetails();
+    updateDetails(options);
   });
 }
 
 injectMingle();
+$(document).on('click', '.mingit-transition', handleTransition);
 
 // re-inject when page transition is detected
 new MutationObserver(injectMingle).observe(document.getElementById('js-repo-pjax-container'), { childList: true });
